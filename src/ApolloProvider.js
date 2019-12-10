c94352ddae227af774d8ff4ea53fab82b61e7de2
Maghin <ahmedshabana0.0@gmail.com>
2019-12-10 18:25:22 +0200

    added proxy and fixed http

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -6,7 +6,7 @@ import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000"
+  uri: "https://shrouded-woodland-38032.herokuapp.com/"
 });
 
 const authLink = setContext(() => {
