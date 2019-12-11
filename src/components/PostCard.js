0e684323ba435b996708786d9a036c61b4391e98
Maghin <ahmedshabana0.0@gmail.com>
2019-12-11 12:07:03 +0200

    fixed issues

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
-import { Button, Card, Icon, Label, Image } from "semantic-ui-react";
-import { Link } from "react-router-dom";
-import moment from "moment";
+import React, { useContext } from 'react';
+import { Button, Card, Icon, Label, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
 
-import { AuthContext } from "../context/auth";
-import LikeButton from "./LikeButton";
-import DeleteButton from "./DeleteButton";
-import MyPopup from "../util/MyPopup";
+import { AuthContext } from '../context/auth';
+import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
+import MyPopup from '../util/MyPopup';
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes }
